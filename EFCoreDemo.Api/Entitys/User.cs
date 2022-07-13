be2b8ecb6fa5110d524c1d6ce41fba12239370a5
MoRanGu <1126146235@qq.com>
2022-07-13 21:17:01 +0800

    first commit

---
+++ EFCoreDemo.Api/Entitys/User.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreDemo.Api.Entitys
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public int Age { get; set; }
+    }
+}
