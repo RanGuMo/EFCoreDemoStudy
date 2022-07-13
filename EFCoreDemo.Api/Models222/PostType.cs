c50070f7d858573e1b65f91d2bed843be1e1415e
MoRanGu <1126146235@qq.com>
2022-07-13 23:45:28 +0800

    VS 中使用DBFirst

---
+++ EFCoreDemo.Api/Models222/PostType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCoreDemo.Api.Models222
+{
+    public partial class PostType
+    {
+        public int Id { get; set; }
+        public string PostType1 { get; set; } = null!;
+        public DateTime CreateTime { get; set; }
+        public int CreateUserId { get; set; }
+    }
+}
